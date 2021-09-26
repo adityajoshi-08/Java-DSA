db33ffdad5b3d982b4043aa4579b4318387cfab9
adityajoshi304 <adityajoshi304@gmail.com>
2021-09-27 00:11:18 +0530

    Some modifications made

--- First/arraysandlist.java
+++ First/arraysandlist.java
@@ -1,7 +1,11 @@
 public class arraysandlist {
     public static void main(String[] args) {
-        int[] ros;
-        ros = new int[5];
-        System.out.println(ros[0]);
+        //Syntax of an array
+        // datatype[] variable_name = new datatype[size]
+        // or
+        // datatype[] variable_name ={andar array daal do}
+
+        int[] arr1 = new int[5]{1, 2, 3, 4, 5};
+//         arr1= new int[]{1, 2, 3, 4, 5};
     }
 }
