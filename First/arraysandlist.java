fcd915d506ff77dea19dc84c6bd290fa64e7d43f
Aditya Joshi <adityajoshi304@gmail.com>
2021-09-26 20:32:05 +0530

    Array commited from MAC

--- First/arraysandlist.java
+++ First/arraysandlist.java
@@ -1,6 +1,7 @@
 public class arraysandlist {
     public static void main(String[] args) {
         int[] ros;
+        //nothing
         ros = new int[5];
         System.out.println(ros[0]);
     }
