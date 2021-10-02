9258354f7e7d58d2de5fc58b4d8cccfdade9ae2a
adityajoshi304 <adityajoshi304@gmail.com>
2021-10-02 19:00:02 +0530

    toCharArray added

--- Linear Search/src/com/aditya/SearchInString.java
+++ Linear Search/src/com/aditya/SearchInString.java
@@ -1,4 +1,12 @@
 package com.aditya;
 
-public class SearchInString {
+//import org.w3c.dom.ls.LSOutput;
+
+import java.util.Arrays;
+
+public class SearchInString  {
+    public static void main(String[] args) {
+        String name = "Aditya";
+        System.out.println(Arrays.toString( name.toCharArray()));
+    }
 }
