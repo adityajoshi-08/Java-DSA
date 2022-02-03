cfddf00fa3b8b3e7c449d22dbf545d1b396b30d1
Aditya Joshi <adityajoshi304@gmail.com>
2022-02-03 20:53:41 +0530

    EMpty

--- Object Oriented Programming/src/intro/Main.java
+++ Object Oriented Programming/src/intro/Main.java
@@ -1,2 +1,25 @@
-package intro;public class Main {
+package intro;
+
+public class Main {
+    public static void main(String[] args) {
+        Student aditya = new Student();
+        Student aditya2 = new Student(2,"Aditya", 100f);
+        System.out.println(aditya2.rno + " "+aditya2.name+ " "+aditya2.marks);
+    }
+}
+class Student{
+    int rno;
+    String name;
+    float marks;
+
+    Student(){
+        this(13,"Aditya", 12.2f);
+    }
+
+//    this is more generalised
+    Student (int rno, String name, float marks){
+        this.rno = rno;
+        this.name = name;
+        this.marks = marks;
+    }
 }
